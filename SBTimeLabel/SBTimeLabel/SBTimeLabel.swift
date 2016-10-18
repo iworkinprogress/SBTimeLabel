f22b25b1536fc2a8fa7c625371a01ad2a3eb336c
Steven Baughman <steve@iworkinprogress.com>
2016-10-18 16:01:12 -0400

    Code cleanup for Stopwatch

--- SBTimeLabel/SBTimeLabel/SBTimeLabel.swift
+++ SBTimeLabel/SBTimeLabel/SBTimeLabel.swift
@@ -29,15 +29,6 @@ class SBTimeLabel: UILabel {
     private var endDate:Date?
     private var elapsedTime:TimeInterval = 0
     
-    private func timeInterval() -> TimeInterval {
-        switch accurracy {
-            case .seconds:
-                return 1.0
-            case .milliseconds:
-                return 0.01
-        }
-    }
-    
     private lazy var dateFormatter:DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = dateFormat
@@ -49,25 +40,21 @@ class SBTimeLabel: UILabel {
         var string:String!
         switch type {
             case .stopwatch:
-                string = dateFormatter.string(from: self.elapsedTimeAsDate())
+                string = dateFormatter.string(from: self.elapsedTimeAsDate)
             case .countdown:
-                string = dateFormatter.string(from: self.elapsedTimeAsDate())
+                string = dateFormatter.string(from: self.elapsedTimeAsDate)
             case .clock:
                 string = dateFormatter.string(from: Date())
         }
         text = string
     }
     
-    private func elapsedTimeAsDate() -> Date {
-        return Date(timeIntervalSince1970: elapsedTime)
-    }
-    
     private func incrementTime() {
-        self.elapsedTime += self.timeInterval()
+        self.elapsedTime += self.timeInterval
         self.updateText()
     }
     
-    //MARK: - States
+    //MARK: - Dynamic Properties
     var isRunning: Bool {
         get {
             if let timer = self.timer {
@@ -78,15 +65,30 @@ class SBTimeLabel: UILabel {
         }
     }
     
+    var elapsedTimeAsDate: Date {
+        get {
+            return Date(timeIntervalSince1970: elapsedTime)
+        }
+    }
+    
+    private var timeInterval: TimeInterval {
+        switch accurracy {
+            case .seconds:
+                return 1.0
+            case .milliseconds:
+                return 0.01
+        }
+    }
+    
     //MARK: - Controls
     func start() {
         timer?.invalidate()
         
-        timer = Timer.scheduledTimer(withTimeInterval: self.timeInterval(), repeats: true, block: { (timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: self.timeInterval, repeats: true, block: { (timer) in
             self.incrementTime()
         })
-        // Set Tolerance as 10% of timer, as suggested by Apple Docs
-        timer?.tolerance = self.timeInterval() / 10.0
+        // Set Tolerance to 10% of timer, as suggested by Apple Docs
+        timer?.tolerance = self.timeInterval / 10.0
         
         startDate = Date()
     }
